2c1dd88887f7a8459dc84505cfc4fcdc2505aa35
aforrester13 <55455905+aforrester13@users.noreply.github.com>
2019-10-27 11:39:54 -0400

    Update trip.js

--- src/trip.js
+++ src/trip.js
@@ -5,14 +5,12 @@ const NumberOfTeachers = parseFloat(readlineSync.question("Teachers: "), 10);
 const BusCapacity = parseFloat(readlineSync.question("Bus capacity: "), 10);
 
 const StudentsAndTeachers = (NumberOfStudents + NumberOfTeachers);
-
 const NumberOfBuses = (StudentsAndTeachers/BusCapacity);
 
 let LeftoverPeople = (StudentsAndTeachers % BusCapacity);
-
 
 const A = NumberOfBuses.toLocaleString("en", {minimumFractionDigits:0, maximumFractionDigits:0});
 const B = LeftoverPeople.toLocaleString("en", {minimumFractionDigits:0, maximumFractionDigits:0});
 
 
-console.log("\n" + A + " bus(es) is (are) needed, with " + B + " passenger(s) on the last bus.");
+console.log("\n" + A + " bus(es) is (are) needed, with " + B + " passenger(s) on the last bus.\n");
