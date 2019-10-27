0ae3cf49af69ba849a5a056ff206775635afb774
aforrester13 <55455905+aforrester13@users.noreply.github.com>
2019-10-27 11:34:21 -0400

    Update area.js

--- src/area.js
+++ src/area.js
@@ -3,6 +3,7 @@ const readlineSync = require("readline-sync");
 const Width = readlineSync.question("\nWidth: ");
 const Length = readlineSync.question("Length: ");
 const Conversion = 25.4;
+
 let w = Width;
 let l = Length;
 let c = w*Conversion;
@@ -11,4 +12,4 @@ let f = c*s;
 let h = Math.round(100*f)/(100);
 let x = h.toLocaleString()
 
-console.log("\nA(n) " + w + "-by-" + l +  "-inch sheet of paper has an area of " + x +   " square millimeter(s).");
+console.log("\nA(n) " + w + "-by-" + l +  "-inch sheet of paper has an area of " + x +   " square millimeter(s).\n");
