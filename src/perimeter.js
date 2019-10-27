593040c1a3f54a0d77895e6908c00db75cdb6ef3
aforrester13 <55455905+aforrester13@users.noreply.github.com>
2019-10-27 11:38:44 -0400

    Update perimeter.js

--- src/perimeter.js
+++ src/perimeter.js
@@ -3,6 +3,7 @@ const readlineSync = require("readline-sync");
 const Width = readlineSync.question("\nWidth: ");
 const Length = readlineSync.question("Length: ");
 const Conversion = 2.54;
+
 let w = Width;
 let l = Length;
 let c = w*Conversion;
@@ -11,4 +12,4 @@ let f = (2*c)+(2*s);
 let h = Math.round(100*f)/(100);
 let x = h.toLocaleString()
 
-console.log("\nA(n) " + w + "-by-" + l +  "-inch sheet of paper has a perimeter of " + x +   " centimeters(s).");
+console.log("\nA(n) " + w + "-by-" + l +  "-inch sheet of paper has a perimeter of " + x +   " centimeters(s).\n");
